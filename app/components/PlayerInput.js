a06401f18bd7d09ce7923e06868a393c6e622b01
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 22:29:12 +0700

    Create PlayerInput basic form

--- app/components/PlayerInput.js
+++ app/components/PlayerInput.js
@@ -2,15 +2,36 @@ const React = require('react')
 const PropTypes = require('prop-types')
 
 class PlayerInput extends React.Component {
-  render() {
-    return ()
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  render () {
+    return (
+      <form className='player-form'>
+        <label className='label-header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input
+          id='username'
+          placeholder='github_username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange} />
+      </form>
+    )
   }
 }
 
 PlayerInput.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func.isRequired 
+  onSubmit: PropTypes.func.isRequired
 }
 
 module.exports = PlayerInput

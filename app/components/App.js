79f29627190e0b7aca0d0850be89fb90cb24fa1d
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 21:58:36 +0700

    Use Switch so we can have Route for Page Not Found

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ const React = require('react')
 const ReactRouter = require('react-router-dom')
 const Router = ReactRouter.BrowserRouter
 const Route = ReactRouter.Route
+const Switch = ReactRouter.Switch
 const Navigation = require('./Navigation')
 const Home = require('./Home')
 const Popular = require('./Popular')
@@ -13,9 +14,18 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Navigation />
-          <Route exact path='/' component={Home} />
-          <Route path='/popular' component={Popular} />
-          <Route path='/battle' component={Battle} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/popular' component={Popular} />
+            <Route path='/battle' component={Battle} />
+            <Route render={function () {
+              return (
+                <div className='container-center'>
+                  <h1>Page Not Found</h1>
+                </div>
+              )
+            }} />
+          </Switch>
         </div>
       </Router>
     )

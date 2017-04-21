f02d78a5bdb5db0463d4912c2db1fa2a31809b04
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 22:10:45 +0700

    Create state for players in Battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,6 +2,21 @@ const React = require('react')
 const Link = require('react-router-dom').Link
 
 class Battle extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      playerOne: {
+        name: '',
+        image: ''
+      },
+      playerTwo: {
+        name: '',
+        image: ''
+      }
+    }
+  }
+
   render () {
     return (
       <div className='container-battle'>

48bbae30a7abe44fb7490fed63284cbd0e86b210
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 23:14:42 +0700

    Complete PlayerPreview, especially onReset

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -10,12 +10,21 @@ const PlayerPreview = (props) => {
           src={props.avatar}
           alt={`Avatar for ${props.username}`}
         />
+        <h3 className='username'>
+          <a href={`https://github.com${props.username}`}>@{props.username}</a>
+        </h3>
+        <a
+          className='reset'
+          onClick={props.onReset.bind(null, props.id)}>
+          Reset Player
+        </a>
       </div>
     </div>
   )
 }
 
 PlayerPreview.propTypes = {
+  id: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
   username: PropTypes.string.isRequired,
   onReset: PropTypes.func.isRequired

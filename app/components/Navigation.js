6af83e63a5b12be69d171c78a8f1e6d6293c6e72
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 21:23:14 +0700

    Create initial Navigation component
    with Link and NavLink

---
+++ app/components/Navigation.js
@@ -0,0 +1,14 @@
+const React = require('react')
+const ReactRouter = require('react-router-dom')
+const Link = ReactRouter.Link
+const NavLink = ReactRouter.NavLink
+
+const Navigation = () => {
+  return (
+    <ul className='nav' />
+    <li></li>
+  </ul>
+  )
+}
+
+module.exports = Navigation

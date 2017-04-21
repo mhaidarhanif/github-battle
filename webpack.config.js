e62e69d4550ba7b2547d27118263b17597307f68
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 21:54:11 +0700

    Use publicPath & historyApiFallback in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,17 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({

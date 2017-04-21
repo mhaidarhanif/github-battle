baafc69b13073fa5f42624a09b06598edafa166a
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-21 21:20:52 +0700

    Create some basic Route in Router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,19 @@
 const React = require('react')
+const ReactRouter = require('react-router-dom')
+const Router = ReactRouter.BrowserRouter
+const Route = ReactRouter.Route
 const Popular = require('./Popular')
 
 class App extends React.Component {
   render () {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Route path='' component={Popular} />
+          <Route path='/popular' component={Popular} />
+          <Route path='/battle' component={Popular} />
+        </div>
+      </Router>
     )
   }
 }

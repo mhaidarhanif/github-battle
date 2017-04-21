c9b85ed43a533123f65ead6d9cf4e79dcfece29a
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-22 02:15:53 +0700

    Create initial reusable Loading component

---
+++ app/components/Loading.js
@@ -0,0 +1,36 @@
+const React = require('react')
+const PropTypes = require('prop-types')
+
+const styles: {
+  content: {
+    textAlign: 'center',
+    fontSize: '2em'
+  }
+}
+
+class Loading extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: props.text,
+      loading: props.loading
+    }
+  }
+
+  render() {
+    return (
+      <p style={styles.content}>{this.state.text}</p>
+    )
+  }
+}
+
+Loading.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+Loading.defaultProps = {
+  text: 'Loading...'
+}
+
+module.exports = Loading
